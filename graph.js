2e1f4cd59972fba394b1040eca9e4418c9da2949
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-13 16:02:57 +0100

    Migrate to D3.js v4

--- graph.js
+++ graph.js
@@ -16,6 +16,34 @@ function whenDocumentLoaded(action) {
 	}
 }
 
+drag = simulation => {
+
+	function dragstarted(d) {
+		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged(d) {
+		d.fx = d3.event.x;
+		d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+		if (!d3.event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
+
+	return d3.drag()
+			.on("start", dragstarted)
+			.on("drag", dragged)
+			.on("end", dragended);
+}
+
+// https://medium.com/@nick3499/d3-scaleordinal-d3-schemecategory10-categorical-ordinal-scale-cab259c4d1e5
+const scale = d3.scaleOrdinal(d3.schemeCategory10);
+
 class NetworkGraph {
 	constructor(figure_element_id, nodes, edges){
 		this.svg = figure_element_id;
@@ -26,55 +54,52 @@ class NetworkGraph {
 	//TODO : integrate code below into class
 }
 
-const width = 960,
-    height = 500;
+const width = 960, height = 500;
 
 const svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
-
-const force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
+    .attr("height", height)
 
 d3.json("data.json", function(json) {
-	force.nodes(json.nodes)
-	      .links(json.edges)
-	      .start();
+
+	var simulation = d3.forceSimulation(json.nodes)
+		.force('link', d3.forceLink()
+			.links(json.edges)
+			.id((d) => d.id))
+		.force('charge', d3.forceManyBody().strength(-100))
+		.force('center', d3.forceCenter(width / 2, height / 2))
 
 	var edge = svg.selectAll(".link")
-				  .data(json.edges)
-				.enter().append("line")
-				  .attr("class", "link")
-				.style("stroke-width", function(d) { return 10*d.weight; });
+		.data(json.edges)
+		.enter().append("line")
+		.attr("class", "link")
+		.style("stroke-width", function(d) { return Math.sqrt(d.weight); });
 
 	var node = svg.selectAll(".node")
-				  .data(json.nodes)
-				.enter().append("g")
-				  .attr("class", "node")
-				  .call(force.drag);
+		.data(json.nodes)
+		.enter().append("g")
+		.attr("class", "node")
+		.attr('fill', (d) => scale(d.group))
+		.call(drag(simulation));
 
 	node.append("circle")
-	  .attr("r","5");
+		.attr("r","5");
 
 	node.append("text")
-	  .attr("dx", 12)
-	  .attr("dy", ".35em")
-	  .text(function(d) { return d.id });
+		.attr("dx", 12)
+		.attr("dy", ".35em")
+		.text(function(d) { return d.id });
 
-	force.on("tick", function() {
+	simulation.on("tick", function() {
 		edge.attr("x1", function(d) { return d.source.x; })
-		    .attr("y1", function(d) { return d.source.y; })
-		    .attr("x2", function(d) { return d.target.x; })
-		    .attr("y2", function(d) { return d.target.y; });
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
 
 		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 	});
 });
 
-
 whenDocumentLoaded(() => {
-	//graph = new NetworkGraph(svg, nodes, edges);
+	// graph = new NetworkGraph(svg, nodes, edges);
 });
